74497b89e8e143c54b9961dd7c04b6e69df52b56
lyuboasenov <lyubo.asenov@gmail.com>
2018-10-12 08:34:14 +0300

    Updated allowed hosts.

--- climbing_guide_api/climbing_guide_api/settings.py
+++ climbing_guide_api/climbing_guide_api/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'rnd%wsoh=9mnq65zx#%v$^-iq9srv5p)85l(o*s1o+pnb8rfu!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '10.0.2.2'
+]
 
 
 # Application definition
