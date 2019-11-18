e0ac46db37520b6714b8a1d6004d65a590a4de98
lyuboasenov <lyubo.asenov@gmail.com>
2019-11-18 07:13:47 +0200

    Add initial in memory identity server 4 web project.

---
+++ src/Climbing.Guide.Api/Climbing.Guide.IDP/DevEnv.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Climbing.Guide.IDP {
+   public static class DevEnv {
+      public static List<TestUser> GetUsers() {
+         return new List<TestUser>() {
+            new TestUser() {
+               SubjectId = "61C49A3B-1206-4F8D-B77F-5B27D65FD771",
+               Username = "lyubo",
+               Password = "pass123$",
+               IsActive = true,
+               Claims = new [] {
+                  new Claim("given_name", "Lyuboslav"),
+                  new Claim("family_name", "Asenov"),
+                  new Claim("role", "super_admin"),
+               }
+            },
+            new TestUser() {
+               SubjectId = "B078ABE6-A4BA-4B33-838A-954BEE3CA726",
+               Username = "ogi",
+               Password = "pass123$",
+               IsActive = true,
+               Claims = new [] {
+                  new Claim("given_name", "Ognyan"),
+                  new Claim("family_name", "Asenov"),
+                  new Claim("role", "climber"),
+               }
+            },
+         };
+      }
+
+      public static IEnumerable<IdentityResource> GetIdentityResources() {
+         return new IdentityResource[] {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+         };
+      }
+
+      public static IEnumerable<ApiResource> GetApiResources() {
+         return new ApiResource[] {
+
+         };
+      }
+
+      public static IEnumerable<Client> GetClients() {
+         return new Client[] {
+
+         };
+      }
+   }
+}
---
+++ src/Climbing.Guide.Api/Climbing.Guide.IDP/Startup.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Climbing.Guide.IDP {
+   public class Startup {
+      public IWebHostEnvironment Environment { get; }
+      public IConfiguration Configuration { get; }
+
+      public Startup(IWebHostEnvironment environment, IConfiguration configuration) {
+         Environment = environment;
+         Configuration = configuration;
+      }
+
+      public void ConfigureServices(IServiceCollection services) {
+         services.AddControllersWithViews();
+
+         services.AddIdentityServer(options => {
+               options.Events.RaiseErrorEvents = true;
+               options.Events.RaiseInformationEvents = true;
+               options.Events.RaiseFailureEvents = true;
+               options.Events.RaiseSuccessEvents = true;
+            }).
+            AddDeveloperSigningCredential().
+            AddTestUsers(DevEnv.GetUsers()).
+            AddInMemoryIdentityResources(DevEnv.GetIdentityResources()).
+            AddInMemoryApiResources(DevEnv.GetApiResources()).
+            AddInMemoryClients(DevEnv.GetClients());
+      }
+
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app) {
+         if (Environment.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+         }
+
+         app.UseStaticFiles();
+
+         app.UseRouting();
+         app.UseIdentityServer();
+         app.UseAuthorization();
+         app.UseEndpoints(endpoints => {
+            endpoints.MapDefaultControllerRoute();
+         });
+      }
+   }
+}
