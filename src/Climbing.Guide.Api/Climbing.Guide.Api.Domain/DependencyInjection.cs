027d9206393a4bf262a3ffe61b810a50ab6eb47e
lyuboasenov <lyubo.asenov@gmail.com>
2019-11-24 22:03:06 +0200

    Misc

--- src/Climbing.Guide.Api/Climbing.Guide.Api.Domain/DependencyInjection.cs
+++ src/Climbing.Guide.Api/Climbing.Guide.Api.Domain/DependencyInjection.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Climbing.Guide.Api.Application.Routes {
    public static class DependencyInjection {
       public static IServiceCollection AddDomain(this IServiceCollection services) {
-         services.AddTransient(typeof(IValueFactory), typeof(ValueFactory));
+         services.AddScoped(typeof(IValueFactory), typeof(ValueFactory));
 
          return services;
       }
