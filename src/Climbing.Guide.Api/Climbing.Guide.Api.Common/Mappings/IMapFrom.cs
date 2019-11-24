cd7b12f9d76ff8aa62640c69bfd33253c6e9f98a
lyuboasenov <lyubo.asenov@gmail.com>
2019-11-24 22:02:26 +0200

    Extract mapping to separate library.

---
+++ src/Climbing.Guide.Api/Climbing.Guide.Api.Common/Mappings/IMapFrom.cs
@@ -0,0 +1,7 @@
+﻿using AutoMapper;
+
+namespace Climbing.Guide.Api.Common.Mappings {
+   public interface IMapFrom<T> {
+      void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+   }
+}
---
+++ src/Climbing.Guide.Api/Climbing.Guide.Api.Common/Mappings/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Climbing.Guide.Api.Common.Mappings {
+   public abstract class MappingProfile : Profile {
+      protected MappingProfile() {
+         ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+      }
+
+      private void ApplyMappingsFromAssembly(Assembly assembly) {
+         var types = assembly.GetExportedTypes()
+             .Where(t => t.GetInterfaces().Any(i =>
+                 i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+             .ToList();
+
+         foreach (var type in types) {
+            var instance = Activator.CreateInstance(type);
+            var methodInfo = type.GetMethod("Mapping");
+            methodInfo?.Invoke(instance, new object[] { this });
+         }
+      }
+   }
+}
