1528b173ad85171df82324cce9fdb99ebb1bbc53
lyuboasenov <lyubo.asenov@gmail.com>
2019-11-27 20:54:14 +0200

    Formating.

--- src/Climbing.Guide.Api/Climbing.Guide.Api.Infrastructure/DependencyInjection.cs
+++ src/Climbing.Guide.Api/Climbing.Guide.Api.Infrastructure/DependencyInjection.cs
@@ -7,7 +7,10 @@ namespace Climbing.Guide.Api.Infrastructure {
    public static class DependencyInjection {
       public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
          services.AddDbContext<DbContext>(options =>
-             options.UseSqlServer(configuration.GetConnectionString("ClimbingGuideConnectionString")));
+             options
+               .UseSqlServer(configuration.GetConnectionString("ClimbingGuideConnectionString"))
+               .EnableDetailedErrors()
+               .EnableSensitiveDataLogging());
 
          services.AddScoped<IDbContext>(provider => provider.GetService<DbContext>());
          services.AddScoped<IFsContext>(provider => provider.GetService<FsContext>());
